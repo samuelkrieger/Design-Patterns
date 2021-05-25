74d5417b23661c2ed2ca9163ff4b31051dcdf3e3
samuelkrieger <79378032+samuelkrieger@users.noreply.github.com>
2021-05-25 14:31:34 -0300

    Update CalculateDiscount.java

--- src/design/chainresponsibility/solution/CalculateDiscount.java
+++ src/design/chainresponsibility/solution/CalculateDiscount.java
@@ -4,9 +4,16 @@ import java.math.BigDecimal;
 
 import design.chainresponsibility.Estimate;
 
+/*Due to the need to not 
+ *know which discount to apply
+ *the chain of responsibility standard is used
+ */
 public class CalculateDiscount {
 	
 	public BigDecimal calculate(Estimate estimate) {
+		/*A chain of possible discount algorithms 
+		 * is created using the inheritance principle
+		 */
 		Discount discount=new DiscountGreaterItem(
 						  new DiscountGreaterItemB(
 						  new NullDiscount()));
